3de523f74b1105e58e3f09c110895b0bd5147c7f
Jack Homan <homan.jack@gmail.com>
2022-01-27 23:29:58 -0500

    finalizer for recurring run

--- controllers/recurringrun_controller.go
+++ controllers/recurringrun_controller.go
@@ -18,6 +18,12 @@ package controllers
 
 import (
 	"context"
+	"fmt"
+	"github.com/johnhoman/go-kfp"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/client-go/tools/record"
+	cu "sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -31,7 +37,14 @@ import (
 type RecurringRunReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
+	record.EventRecorder
+
+	api kfp.Interface
 }
+
+const (
+	RecurringRunFinalizer = "kfp.jackhoman.com/delete-recurring-run"
+)
 
 //+kubebuilder:rbac:groups=kfp.jackhoman.com,resources=recurringruns,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=kfp.jackhoman.com,resources=recurringruns/status,verbs=get;update;patch
@@ -39,17 +52,61 @@ type RecurringRunReconciler struct {
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the RecurringRun object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.10.0/pkg/reconcile
 func (r *RecurringRunReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	logger := log.FromContext(ctx)
+	k8s := client.NewNamespacedClient(r.Client, req.Namespace)
 
-	// TODO(user): your logic here
+	instance := &kfpv1alpha1.RecurringRun{}
+	if err := k8s.Get(ctx, req.NamespacedName, instance); err != nil {
+		logger.Info("instance not found", "error", err.Error())
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	if !instance.GetDeletionTimestamp().IsZero() {
+		// RecurringRun is being deleted
+		if cu.ContainsFinalizer(instance, RecurringRunFinalizer) {
+			_, err := r.api.GetJob(ctx, &kfp.GetOptions{Name: instance.GetName()})
+			if err != nil {
+				if !kfp.IsNotFound(err) {
+					logger.Error(err, "unable to get job")
+					return ctrl.Result{}, err
+				}
+			} else {
+				// err == nil
+				// if err := r.api.DeleteJob(ctx, &kfp.DeleteOptions{ID: job.ID}); err != nil {
+				// 	logger.Error(err, "unable to delete recurring run")
+				// 	return ctrl.Result{}, err
+				// }
+				r.Eventf(instance, corev1.EventTypeNormal, "Deleted", fmt.Sprintf(
+					"Removed recurring run %s", instance.Status.ID,
+				))
+			}
+			patch := client.MergeFrom(instance.DeepCopy())
+			cu.RemoveFinalizer(instance, RecurringRunFinalizer)
+			if err := k8s.Patch(ctx, instance, patch); err != nil {
+				logger.Error(err, "unable to remove finalizer")
+				return ctrl.Result{}, err
+			}
+			logger.Info("removed finalizer")
+		}
+
+		return ctrl.Result{}, nil
+	}
+	if !cu.ContainsFinalizer(instance, RecurringRunFinalizer) {
+		patch := &unstructured.Unstructured{Object: map[string]interface{}{
+			"metadata": map[string]interface{}{
+				"finalizers": []string{RecurringRunFinalizer},
+			},
+		}}
+		patch.SetGroupVersionKind(instance.GroupVersionKind())
+		patch.SetName(instance.GetName())
+		if err := k8s.Patch(ctx, patch, client.Apply, FieldOwner); err != nil {
+			logger.Error(err, "unable to add finalizer")
+			return ctrl.Result{}, err
+		}
+		logger.Info("added finalizer")
+	}
+
 
 	return ctrl.Result{}, nil
 }
--- controllers/recurringrun_controller_test.go
+++ controllers/recurringrun_controller_test.go
@@ -2,7 +2,10 @@ package controllers
 
 import (
 	"encoding/json"
+	kfpv1alpha1 "github.com/johnhoman/kfp-releaser/api/v1alpha1"
+	"k8s.io/apimachinery/pkg/types"
 	"os"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"strings"
 
 	httptransport "github.com/go-openapi/runtime/client"
@@ -50,6 +53,13 @@ var _ = Describe("RecurringRunController", func() {
 			EventRecorder: it.GetEventRecorderFor("kfp-releaser.controller-test"),
 		}).SetupWithManager(it)
 		Expect(err).ToNot(HaveOccurred())
+		err = (&RecurringRunReconciler{
+			Client:        it.GetClient(),
+			Scheme:        it.GetScheme(),
+			EventRecorder: it.GetEventRecorderFor("kfp-releaser.controller-test"),
+			api:     service,
+		}).SetupWithManager(it)
+		Expect(err).ToNot(HaveOccurred())
 
 		it.StartManager()
 
@@ -57,4 +67,86 @@ var _ = Describe("RecurringRunController", func() {
 		Expect(err).ShouldNot(HaveOccurred())
 	})
 	AfterEach(func() { it.StopManager() })
+	FWhen("a pipeline exists", func() {
+		var pipeline *kfpv1alpha1.Pipeline
+		var version *kfpv1alpha1.PipelineVersion
+		var name string
+		var key types.NamespacedName
+		var versionKey types.NamespacedName
+		BeforeEach(func() {
+			name = "whalesay"
+			key = types.NamespacedName{Name: name}
+			versionKey = types.NamespacedName{Name: name + "-v1"}
+
+			pipeline = &kfpv1alpha1.Pipeline{}
+			pipeline.SetName(name)
+			pipeline.Spec.Description = "Whalesay Pipeline"
+			it.Eventually().Create(pipeline).Should(Succeed())
+			it.Eventually().GetWhen(key, pipeline, func(obj client.Object) bool {
+				return len(obj.(*kfpv1alpha1.Pipeline).Status.ID) > 0
+			}).Should(Succeed())
+
+			version = &kfpv1alpha1.PipelineVersion{}
+			version.SetName(versionKey.Name)
+			version.Spec.Workflow.Raw = raw
+			version.Spec.Pipeline = pipeline.GetName()
+			version.Spec.Description = pipeline.Spec.Description
+
+			it.Eventually().Create(version).Should(Succeed())
+			it.Eventually().GetWhen(versionKey, version, func(obj client.Object) bool {
+				return len(obj.(*kfpv1alpha1.PipelineVersion).Status.ID) > 0
+			}).Should(Succeed())
+		})
+		AfterEach(func() {
+			// Just call delete, pass or fail doesn't matter all that much
+			it.Expect().Delete(version).Should(Or(Succeed(), Not(Succeed())))
+			it.Expect().Delete(pipeline).Should(Or(Succeed(), Not(Succeed())))
+		})
+		When("the recurring run exists", func() {
+			var recurringRun *kfpv1alpha1.RecurringRun
+			BeforeEach(func() {
+				recurringRun = &kfpv1alpha1.RecurringRun{}
+				recurringRun.SetName(versionKey.Name)
+				recurringRun.SetFinalizers([]string{"keep"})
+				recurringRun.Spec.Schedule = kfpv1alpha1.RecurringRunSchedule{Cron: "* * * * *"}
+				recurringRun.Spec.VersionRef = version.GetName()
+				it.Eventually().Create(recurringRun).Should(Succeed())
+			})
+			AfterEach(func() {
+				it.Expect().Delete(recurringRun).Should(Or(Succeed(), Not(Succeed())))
+			})
+			When("the recurring run is not being deleted", func() {
+				When("it has a finalizer", func() {
+					BeforeEach(func() {
+						instance := &kfpv1alpha1.RecurringRun{}
+						it.Eventually().GetWhen(versionKey, instance, func(obj client.Object) bool {
+							return len(obj.GetFinalizers()) > 1
+						}).Should(Succeed())
+					})
+					It("should contain the correct finalizer", func() {
+						instance := &kfpv1alpha1.RecurringRun{}
+						it.Eventually().Get(versionKey, instance).Should(Succeed())
+						Expect(instance.GetFinalizers()).Should(ContainElement(RecurringRunFinalizer))
+					})
+				})
+				When("it does not have a finalizer", func() {
+
+				})
+			})
+			When("the recurring run is being deleted", func() {
+				BeforeEach(func() {
+					it.Expect().Delete(recurringRun).Should(Succeed())
+					it.Eventually().GetWhen(versionKey, &kfpv1alpha1.RecurringRun{}, func(obj client.Object) bool {
+						return len(obj.GetFinalizers()) == 1
+					}).Should(Succeed())
+				})
+				It("should remove the finalizer", func() {
+					instance := &kfpv1alpha1.RecurringRun{}
+					it.Eventually().Get(versionKey, instance).Should(Succeed())
+					Expect(instance.GetFinalizers()).ShouldNot(ContainElement(RecurringRunFinalizer))
+				})
+			})
+		})
+	})
+
 })

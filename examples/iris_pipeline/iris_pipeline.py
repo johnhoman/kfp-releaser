cc9c07ad0b2201fc5dd7375666d662b4fbfb7953
Jack Homan <homan.jack@gmail.com>
2022-02-06 02:23:38 -0500

    changing to example iris pipeline

--- examples/iris_pipeline/iris_pipeline.py
+++ examples/iris_pipeline/iris_pipeline.py
@@ -8,7 +8,10 @@ from kfp.v2 import dsl
 
 
 @component(packages_to_install=['sklearn'], base_image='python:3.9')
-def iris_sgdclassifier(test_samples_fraction: float, metrics: Output[ClassificationMetrics]):
+def iris_sgdclassifier(
+    test_samples_fraction: float,
+    metrics: Output[ClassificationMetrics],
+):
     from sklearn import datasets, model_selection
     from sklearn.linear_model import SGDClassifier
     from sklearn.metrics import confusion_matrix
@@ -19,19 +22,78 @@ def iris_sgdclassifier(test_samples_fraction: float, metrics: Output[Classificat
 
     classifier = SGDClassifier()
     classifier.fit(train_x, train_y)
-    predictions = model_selection.cross_val_predict(classifier, train_x, train_y, cv=3)
+    predictions = model_selection.cross_val_predict(
+        classifier,
+        train_x,
+        train_y,
+        cv=3,
+    )
     metrics.log_confusion_matrix(
         ['Setosa', 'Versicolour', 'Virginica'],
         confusion_matrix(train_y, predictions).tolist()
     )
 
 
-@dsl.pipeline(name='metrics-visualization-pipeline')
-def metrics_visualization_pipeline():
-    iris_sgdclassifier_op = iris_sgdclassifier(test_samples_fraction=0.3)
+@dsl.pipeline(name="iris-classifier")
+def iris_classifier(test_sample_fraction: float = 0.3):
+    iris_sgdclassifier_op = iris_sgdclassifier(
+        test_samples_fraction=test_sample_fraction,
+    )
 
 
 if __name__ == "__main__":
-    from kfp.compiler import Compiler
+    import os
+    import tempfile
+    import pathlib
+
     import kfp
-    Compiler(mode=kfp.dsl.PipelineExecutionMode.V2_COMPATIBLE).compile(metrics_visualization_pipeline, package_path=__file__.replace(".py", ".yaml"))
+    from kfp.compiler import Compiler
+    from yaml import safe_load, safe_dump
+
+    def manifest(pipeline_func):
+
+        ref = os.environ.get("GITHUB_REF_NAME", "latest")
+        name = pipeline_func.__qualname__
+        with tempfile.NamedTemporaryFile(mode="w+t", suffix=".yaml") as open_file:
+            Compiler(
+                mode=kfp.dsl.PipelineExecutionMode.V2_COMPATIBLE,
+            ).compile(pipeline_func=pipeline_func, package_path=open_file.name)
+            with open(open_file.name, mode="rt") as open_file2:
+                workflow = open_file2.read()
+
+        name = name.replace("_", "-").lower()
+        version_name = name + "-" + ref
+
+        version = {
+            "apiVersion": "kfp.jackhoman.com/v1alpha1",
+            "kind": "PipelineVersion",
+            "metadata": {"name": version_name},
+            "spec": {
+                "pipeline": name,
+                "description": f"pipeline release {ref}",
+                "workflow": safe_load(workflow),
+            }
+        }
+        this_dir = pathlib.Path(__file__).parent
+        this_dir.joinpath("pipeline").mkdir(parents=True, exist_ok=True)
+        this_dir.joinpath("versions").mkdir(parents=True, exist_ok=True)
+        base = this_dir.joinpath("pipeline", f"{name}.yaml")
+        path = this_dir.joinpath("versions", f"{version_name}.yaml")
+
+        with path.open(mode="wt") as open_file:
+            safe_dump(version, stream=open_file, default_flow_style=False)
+
+        if not base.exists():
+            pipeline = {
+                "apiVersion": "kfp.jackhoman.com/v1alpha1",
+                "kind": "Pipeline",
+                "metadata": {"name": name},
+                "spec": {
+                    "description": "pipeline declaration",
+                }
+            }
+            base.parent.mkdir(parents=True, exist_ok=True)
+            with base.open(mode="wt") as open_file:
+                safe_dump(pipeline, stream=open_file, default_flow_style=False)
+
+    manifest(pipeline_func=iris_classifier)
